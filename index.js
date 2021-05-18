8d266b91e7aec4efdf46a471c214bfbfc62a0231
Pedro Victor <pvsbenevides197@gmail.com>
2021-05-18 16:27:12 -0300

    Fixed port

--- index.js
+++ index.js
@@ -62,9 +62,9 @@ app.use('/nota', notas_route)
 
 
 //Outros
-const PORT = precess.env || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-	console.log("Servidor rodando na porta ${PORT}.")
+	console.log(`Servidor rodando na porta ${PORT}.`)
 })
 
 
