eb89b5f284c7032223a287380cae17d2a4473989
Pedro Victor <pvsbenevides197@gmail.com>
2021-05-18 16:01:43 -0300

    Added remote db

--- database/config.js
+++ database/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "0818",
-    DB: "handson",
+    HOST: "us-cdbr-east-03.cleardb.com",
+    USER: "bda20c129ff8d1",
+    PASSWORD: "1c8f07c4",
+    DB: "heroku_87c764b7a7825d1",
     dialect: "mysql",
   };
